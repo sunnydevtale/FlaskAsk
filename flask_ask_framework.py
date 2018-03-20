a79c86ae98c39ad8b376114104edea63b1060911
Devtale <417353@cognizant.com>
2018-03-20 17:28:42 +0530

    Fix print statment

--- flask_ask_framework.py
+++ flask_ask_framework.py
@@ -83,5 +83,5 @@ def get_quote():
 if __name__ == '__main__':
     #app.run(debug=True)
     port = int(os.getenv('PORT', 5000))
-    print "Starting app on port %d" % port
+    print ("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
